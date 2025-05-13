c16982309bdada9917f13435c566d1d665abb99b
chelsangya <csangya@gmail.com>
2025-05-13 14:30:09 +0545

    initial project with login, register UI

---
+++ src/demoproject37a/DemoProject37A.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demoproject37a;
+
+import demoproject37a.view.EntryView;
+
+/**
+ *
+ * @author sangyakoirala
+ */
+public class DemoProject37A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        EntryView view = new EntryView();
+        view.setVisible(true);
+    }
+    
+}
