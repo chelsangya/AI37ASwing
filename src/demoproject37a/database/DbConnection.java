c74ee68323c508add4969b7bc550bd7f227213c7
chelsangya <csangya@gmail.com>
2025-05-13 16:30:41 +0545

    fixed package

--- src/demoproject37a/database/DbConnection.java
+++ src/demoproject37a/database/DbConnection.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package database;
+package demoproject37a.database;
 import java.sql.*;
 /**
  *
